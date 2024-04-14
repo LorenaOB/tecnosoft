8a9e94c697914d2d6cc807eb27cd29a9215fa574
Lorena Ortiz <lorenaortizb@gmail.com>
2024-04-14 18:25:12 -0500

    Update 14/04/2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,18 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PlanificationPageComponent } from './pages/planification-page/planification-page.component';
 import { RequirementsAnalysisPageComponent } from './pages/requirements-analysis-page/requirements-analysis-page.component';
 import { UiuxPageComponent } from './pages/uiux-page/uiux-page.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 export const routes: Routes = [
-  {
-    path: '', component: HomePageComponent },
-  { path: 'contact', component: ContactPageComponent },
+  {path: '', component: HomePageComponent },
+  {path:'contact', component: ContactPageComponent },
   {path: 'requirements', component: RequirementsAnalysisPageComponent},
   {path: 'planification', component: PlanificationPageComponent},
   {path: 'uiux', component: UiuxPageComponent},
+  {path: 'about-us', component: AboutUsPageComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'sign-in', component: SignInComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
